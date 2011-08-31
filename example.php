6ab00111f40a2eaca274ab14b6b6c2426113f949
Wouter Snels <nospam@ofloo.net>
2011-08-31 07:06:07 +0000

    updated example, ..
    git-svn-id: https://narf.ofloo.net/svn/ip2c.php/trunk@7 c0290f3f-73d1-e011-b0ae-001cc0bfa30e

--- example.php
+++ example.php
@@ -1,18 +1,32 @@
 <?php
-  // SAMPLE
-  $test = new ip2c ();
-  if ($test->locate ()) {
-    list ($country) = $test->country ();
-    list ($ip) = $test->address ();
-    echo ($ip . " " . $country . "\n");
-  }
-  $test = null;
+
+  /*
+   * Sample code
+   */
+
+  require_once ("ip2c.class.php");
+
+
+  /*
+   * leaving ip2c () emtpy will make it look up the ip that connects to the webserver
+   */
 
   $test = new ip2c ();
-  if ($test->locate ("::")) {
-    list ($country) = $test->country ();
-    list ($ip) = $test->address ();
-    echo ($ip . " " . $country . "\n");
-  }
+  $test->locate ();
+  list ($ip) = $test->address ();
+  list ($country) = $test->country ();
+  echo ($ip . ": " . $country . "\n");
   $test = null;
+
+  /*
+   * leaving ip2c ("::") lookup on localhost
+   */
+
+  $test = new ip2c ();
+  $test->locate ("::");
+  list ($ip) = $test->address ();
+  list ($country) = $test->country ();
+  echo ($ip . ": " . $country . "\n");
+  $test = null;
+
 ?>
