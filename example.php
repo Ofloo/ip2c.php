86fc27f23782c1b1b63025cd9169c7733cfcaae7
Wouter Snels <nospam@ofloo.net>
2011-08-31 06:48:47 +0000

    updated sample to a more proper sample
    git-svn-id: https://narf.ofloo.net/svn/ip2c.php/trunk@4 c0290f3f-73d1-e011-b0ae-001cc0bfa30e

--- example.php
+++ example.php
@@ -2,15 +2,17 @@
   // SAMPLE
   $test = new ip2c ();
   if ($test->locate ()) {
+    list ($country) = $test->country ();
     list ($ip) = $test->address ();
-    echo ($ip . "\n");
+    echo ($ip . " " . $country . "\n");
   }
   $test = null;
 
   $test = new ip2c ();
   if ($test->locate ("::")) {
+    list ($country) = $test->country ();
     list ($ip) = $test->address ();
-    echo ($ip . "\n");
+    echo ($ip . " " . $country . "\n");
   }
   $test = null;
 ?>
