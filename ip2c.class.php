6f2a880514c32074f1f9aaceadaf944655693009
Wouter Snels <nospam@ofloo.net>
2015-11-07 13:42:27 +0100

    updated headers and readme

--- ip2c.class.php
+++ ip2c.class.php
@@ -19,7 +19,7 @@
    * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
    * MA 02110-1301, USA.
    *
-   * SVN: https://narf.ofloo.net/svn/ip2c.php/
+   * GIT: https://github.com/Ofloo/ip2c.php
    *
    */
 
